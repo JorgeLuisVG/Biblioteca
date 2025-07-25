8548589446c6b3b978d5ae0f04fd1f15273d1741
Jorge <jorgeluisvg0500@gmail.com>
2025-07-25 10:12:27 -0600

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -2,6 +2,25 @@ print("Biblioteca")
 
 #título, autor, género, año
 
+def Libro():
+    def generoLibro():
+        print("1 = Narrativo")
+        print("2 = Lirico")
+        print("3 = Dramatico")
+        print("4 = Didactico")
+        print("5 = Ficcion")
+        print("6 = Cientifico")
+        print("7 = Infantil")
+        print("8 = Comic")
+        print("9 = Educativo")
+    nombreLibro = input("ingrese elnombre del libro")
+    nombreAutor = input("Ingrese el nombre del autor")
+    genero = generoLibro()
+    año = int(input("Ingrese el año de publicacion"))
+
+
+    
+
 Libros = []
 
 while True:
